b340b84078b79ecc656edf168c444d573c5a7c7d
pilar6195 <pilar6195@yahoo.com>
2016-08-27 16:56:51 -0500

    Test for Favorites

--- js/favorites.js
+++ js/favorites.js
@@ -3,37 +3,56 @@
 var background = chrome.extension.getBackgroundPage();
 
 background.storage.get(function(items) {
+	
 	var favorites = items.favorites;
 	
 	$.each(favorites, function(index, value) {
-		$('#favorites-list').append('<div>['+ ('0' + index).slice(-2) + '] ' + value.artist + ' - ' + value.song +'</div>');
+		$('#favorites-list tbody').append('<tr id='+index+' class="item"><td>'+index+'</td><td>'+value.artist+'</td><td>'+value.song+'</td><td><a href="javascript:void(0)" class="remove" data-index='+index+'>Remove</a></td></tr>');
 	});
 	
 	$('#text-search').on('keyup', function() {
-		$('#favorites-list').empty();
-		$.each(search( $('[name="search"]:checked').val(), this.value ), function(index, value) {
-			$('#favorites-list').append('<div>['+ ('0' + index).slice(-2) + '] ' + value.artist + ' - ' + value.song +'</div>');
+		$('.item').hide();
+		$.each( search( $('[name="search"]:checked').val(), this.value ), function() {
+			$('#' + this.index).show();
 		});
 	});
 	
 	$('[name="search"]').change(function() {
+		$('.item').hide();
+		$.each(search( this.value, $('#text-search').val() ), function() {
+			$('#' + this.index).show();
+		});
+	});
+	
+	/* $('#text-search').on('keyup', function() {
+		$('#favorites-list').empty();
+		$.each(search( $('[name="search"]:checked').val(), this.value ), function(index, value) {
+			$('#favorites-list').append('<div>['+ ('0' + index).slice(-2) + '] ' + value.artist + ' - ' + value.song +'</div>');
+		});
+	}); */
+	
+	/* $('[name="search"]').change(function() {
 		$('#favorites-list').empty();
 		$.each(search( this.value, $('#text-search').val() ), function(index, value) {
 			$('#favorites-list').append('<div>['+ ('0' + index).slice(-2) + '] ' + value.artist + ' - ' + value.song +'</div>');
 		});
-	});
+	}); */
 	
 	function search(type, value) {
 		if (typeof type !== 'undefined' && typeof value !== 'undefined') {
 			if (type === 'artist') {
-				return $.grep(favorites, function(search) {
-					if ( search.artist.toLowerCase().indexOf( value.toLowerCase() ) > -1 )
+				return $.grep(favorites, function(search, index) {
+					if ( search.artist.toLowerCase().indexOf( value.toLowerCase() ) !== -1 ) {
+						search.index = index
 						return search;
+					}
 				});
 			} else if (type === 'song') {
-				return $.grep(favorites, function(search) {
-					if ( search.song.toLowerCase().indexOf( value.toLowerCase() ) > -1 )
+				return $.grep(favorites, function(search, index) {
+					if ( search.song.toLowerCase().indexOf( value.toLowerCase() ) > -1 ) {
+						search.index = index
 						return search;
+					}
 				});
 			} else {
 				throw Error('Search term must either be "Artist" or "Song".');
@@ -41,5 +60,30 @@ background.storage.get(function(items) {
 		} else
 			throw Error('Must pass search type and term. Search type can be either "Artist" or "Song".');
 	}
-	
 });
+
+$(document).on('click', '.remove', function() {
+	var index = $(this).attr('data-index');
+	background.storage.get(function(items) {
+		items.favorites.splice(index, 1);
+		background.storage.set({ favorites: items.favorites });
+	});
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	for (key in changes) {
+		if ( key == "favorites" ) {
+			$('.item').remove();
+			$.each(changes[key].newValue, function(index, value) {
+				$('#favorites-list tbody').append('<tr id='+index+' class="item"><td>'+index+'</td><td>'+value.artist+'</td><td>'+value.song+'</td><td><a href="javascript:void(0)" class="remove" data-index='+index+'>Remove</a></td></tr>');
+			});
+		}
+	}
+});
+
+function sort(a, b) {
+	if (typeof a !== 'undefined') {
+		a.sort();
+	}
+} 
+
