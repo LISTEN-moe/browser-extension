2ac82d4fd12468b1987a43594eca7b86f99bcc69
pilar6195 <pilar6195@gmail.com>
2017-08-29 22:14:29 -0500

    Changes to websocket stuff and popup.js
    Moved mostly everything from the popup.js file to the background.js file. Websocket is always connected in the background allowing for Now Playing notifications. Also added togglable options.

--- js/background.js
+++ js/background.js
@@ -1,34 +1,66 @@
-// Shortcut for chrome.storage.local api
+/* Shortcut for chrome.storage.local api */
 var storage = chrome.storage.local;
 
-// Create player
+/* Defaults */
+var defaults = {
+	volume: 50,
+	enableAutoplay: false,
+	enableNotifications: true,
+	enableEventNotifications: true,
+	authToken: ''
+}
+
+chrome.runtime.onInstalled.addListener(function(details) {
+
+	if (details.reason === 'update' && details.previousVersion === '2017.5.28.1') storage.clear();
+
+	if (details.reason === 'update') {
+
+		notifications.create('LISTEN.moe', `Extension has updated to v${chrome.runtime.getManifest().version}`);
+	
+	}
+
+});
+
+/* Create player */
 var player = $('<audio id="listen-moe" autoplay>').appendTo('body')[0];
 
-// Gets stored values if any and applies them
-storage.get(function(items) {
-	if (items.ListenMoeVol) radio.setVol(items.ListenMoeVol);
-	if (items.ListenMoeAutoPlay) radio.enable();
+var storageItems = {};
+
+/* Gets stored values if any and applies them */
+storage.get(defaults, function(items) {
+	if (items.volume) radio.setVol(items.volume);
+	if (items.enableAutoplay) radio.enable();
+	storageItems = items;
+});
+
+/* If the token is updated, update the variable here and attempt to authenticate with the WS */
+chrome.storage.onChanged.addListener(function(changes) {
+	for (var item in changes) {
+		storageItems[item] = changes[item].newValue;
+		if (item === 'authToken') radio.socket.sendToken();
+	}
 });
 
 var radio = {
 	enable: function() {
-		player.setAttribute('src', 'https://listen.moe/stream');
+		return player.setAttribute('src', 'https://listen.moe/stream');
 	},
 	disable: function() {
-		player.setAttribute('src', '');
+		return player.setAttribute('src', '');
 	},
 	toggle: function() {
-		radio.isPlaying() ? radio.disable() : radio.enable();
+		return radio.isPlaying() ? radio.disable() : radio.enable();
 	},
 	isPlaying: function() {
 		return player.paused ? false : true;
 	},
-	setVol: function(a) {
-		if (typeof a !== 'undefined' && Number.isInteger(a) ) {
-			if ( (a / 100) >= 0 && (a / 100) <= 1 ) {
-				player.volume = a / 100;
-				storage.set({ListenMoeVol: a});
-				return 'Set Volume to', a;
+	setVol: function(vol) {
+		if (typeof vol !== 'undefined' && Number.isInteger(vol) ) {
+			if ((vol / 100) >= 0 && (vol / 100) <= 1 ) {
+				player.volume = vol / 100;
+				storage.set({ volume: vol });
+				return 'Set Volume to', vol;
 			} else {
 				throw Error('Value must be between 0 and 100!');
 			}
@@ -38,37 +70,215 @@ var radio = {
 	},
 	getVol: function() {
 		return player.volume * 100;
+	},
+	data: {},
+	socket: {
+		ws: null,
+		event: new Event('songChanged'),
+		data: {
+			eventInProgress: false,
+			lastSongID: -1
+		},
+		init: function() {
+
+			radio.socket.ws = new WebSocket('wss://listen.moe/api/v2/socket');
+
+			radio.socket.ws.onopen = function() {
+				console.log('Alright we got a connection. \\o/');
+				setTimeout(radio.socket.sendToken, 1000);
+			}
+
+			radio.socket.ws.onerror = function() {
+				console.log('Uhh. Something happened');
+			}
+
+			radio.socket.ws.onclose = function() {
+				console.log('Welp. The connection was closed :(');
+				console.log('Reconnecting...');
+				radio.socket.data.lastSongID = -1;
+				setTimeout(radio.socket.init, 10000);
+			};
+
+			radio.socket.ws.onmessage = function(response) {
+				if (response.data !== '') {
+					
+					var data;
+					try { data = JSON.parse(response.data); }
+					catch (err) { console.error(err); }
+					if (!data || data.reason) return;
+					
+					console.log(data);
+					
+					radio.data = data;
+
+					/* Emit event for Popup.js */
+					window.dispatchEvent(radio.socket.event);
+
+					/* Display now playing when song changes
+						These checks are to make sure the info doesn't display when:
+						- the websocket first connects
+						- the websocket drops then reconnects
+						- the websocket returns the extended info
+					*/
+					if (data.song_id !== radio.socket.data.lastSongID) {
+
+						/* Check if event is in progress */
+						if (/\s/g.test(data.requested_by) && storageItems.enableEventNotifications) {
+
+							if (!radio.socket.data.eventInProgress)
+								notifications.create(`🎉 ${data.requested_by} has started! 🎉`, radio.data.song_name, radio.data.artist_name);
+							else
+								notifications.create(`🎉 ${data.requested_by} 🎉`, radio.data.song_name, radio.data.artist_name);				
+
+							radio.socket.data.eventInProgress = true;
+
+						} else {
+
+							if (radio.socket.data.lastSongID !== -1 && radio.isPlaying() && storageItems.enableNotifications)
+								notifications.create('Now Playing', radio.data.song_name, radio.data.artist_name);
+
+							radio.socket.data.eventInProgress = false;
+
+						}					
+
+						radio.socket.data.lastSongID = data.song_id;
+
+					}
+
+				}
+			};
+
+		},
+		sendToken: function() {
+			if (storageItems.authToken) {
+				$.ajax({
+					url: "https://listen.moe/api/user",
+					type: 'GET',
+					headers: { 'Authorization': 'Bearer ' + storageItems.authToken },
+					success: function(response) {
+						radio.socket.ws.send(JSON.stringify({ token: storageItems.authToken }))
+					},
+					error: function(error) { console.error(error); }
+				});
+			}
+
+		}
+	},
+	toggleFavorite: function(song) {
+		return new Promise(function(resolve, reject) {
+			$.ajax({
+				url: 'https://listen.moe/api/songs/favorite',
+				type: 'POST',
+				headers: { 'Authorization': 'Bearer ' + storageItems.authToken },
+				contentType: 'application/json;charset=utf-8',
+				data: JSON.stringify({ song }),
+				success: function (response) {
+					if (response.success) {
+						resolve(response.favorite);
+						/* Update the info set in the background page. Also making sure the song hasn't changed while the request was being made */
+						if (radio.data.song_id === song)
+							radio.data.extended.favorite = !radio.data.extended.favorite;
+					} else {
+						reject(error);
+					}
+				},
+				error: function (error) {
+					reject(error);
+				}
+			});
+		});
 	}
 };
 
+radio.socket.init();
+
 chrome.commands.onCommand.addListener(function(command) {
-	console.log(command);
-	if (command === 'toggle_radio') radio.toggle();
-	else if (command === 'vol_up') (radio.getVol() > 95) ? radio.setVol(100) : radio.setVol(Math.floor(radio.getVol() + 5));
-	else if (command === 'vol_down') (radio.getVol() < 5) ? radio.setVol(0) : radio.setVol(Math.floor(radio.getVol() - 5));
+	if (command === 'toggle_radio') 
+		radio.toggle();
+	else if (command === 'vol_up') 
+		(radio.getVol() > 95) ? radio.setVol(100) : radio.setVol(Math.floor(radio.getVol() + 5));
+	else if (command === 'vol_down') 
+		(radio.getVol() < 5) ? radio.setVol(0) : radio.setVol(Math.floor(radio.getVol() - 5));
+	else if (command === 'now_playing') {
+		var npText = /\s/g.test(radio.data.requested_by) ? `🎉 ${radio.data.requested_by} 🎉` : 'Now Playing'; 
+		notifications.create(npText, radio.data.song_name, radio.data.artist_name, false, (radio.data.extended ? true : false));
+	}
 });
 
-// Modify Request Header to change UserAgent
+/* Modify Request Header to change UserAgent */
 if (typeof InstallTrigger === 'undefined') {
 	chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-		// We don't want this to affect all headers sent. Only the ones coming from the background or popup pages.
-		// All requests coming from the background or popup pages will have a Tab Id of -1. Everything else will have a browser generated Tab Id.
 		if (details.tabId === -1) {
-			for (var i = 0; i < details.requestHeaders.length; ++i) {
-				if (details.requestHeaders[i].name === 'User-Agent') {
-					details.requestHeaders[i].value = `Chrome Extension - ${chrome.runtime.getManifest().name}/v${chrome.runtime.getManifest().version}`;
-					break;
-				}
+			for (var header of details.requestHeaders) {
+				if (header.name === 'User-Agent') header.value = `${chrome.runtime.getManifest().name} Chrome Extension v${chrome.runtime.getManifest().version}`
 			}
 		}
-		return {requestHeaders: details.requestHeaders};
-	}, {urls: ["*://listen.moe/*"]}, ["blocking", "requestHeaders"]);
+		return {requestHeaders: details.requestHeaders}
+	}, { urls: [ "*://listen.moe/api/*", "*://listen.moe/stream" ] }, ["blocking", "requestHeaders"]);
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 	if (request.type === 'open') {
-		if (request.value === 'keyshortcuts') chrome.tabs.create({url: 'chrome://extensions/configureCommands'});
+		if (request.value === 'keyshortcuts') chrome.tabs.create({ url: 'chrome://extensions/configureCommands' });
 	}
 
-})
+});
+
+var notifications = {
+	"create": function(title, message, altText, sticky, showFavoriteButton) {
+
+		if (!title || !message) return;
+
+		var notificationContent = {
+			type: 'basic',
+			title, message,
+			iconUrl: 'images/logo128x128.png',
+			requireInteraction: sticky || false
+		};
+
+		if (altText && typeof altText === 'string') notificationContent.contextMessage = altText;
+		
+		var buttonFuntions = [];
+
+		if (showFavoriteButton) {
+			notificationContent.buttons = [{ title: radio.data.extended.favorite ? 'Remove from Favorites' : 'Add to Favorites' }]
+		}
+
+		var id = 'notification_' + Date.now();
+
+		chrome.notifications.create(id, notificationContent);
+
+		return id;
+
+	},
+	"update": function(id, options) {
+
+		chrome.notifications.update(id, options);
+
+	},
+	"clear": function(id, timeout) {
+
+		setTimeout(function() {
+			chrome.notifications.clear(id);
+		}, timeout || 0);
+
+	}
+};
+
+chrome.notifications.onButtonClicked.addListener(function(id, index) {
+	radio.toggleFavorite(radio.data.song_id).then(function(favorited) {
+		notifications.clear(id);
+		if (favorited)
+			notifications.create('Favorites Updated!', `Added ${radio.data.song_name} to favorites!`);
+		else
+			notifications.create('Favorites Updated!', `Removed ${radio.data.song_name} from favorites!`);
+	}).catch(function(error) {
+		notifications.clear(id);
+		notifications.create('Error Updating Favorites!', 'An error has occured while trying to update your favorites!');
+	});
+});
+
+chrome.notifications.onClicked.addListener(function(id) {
+	chrome.notifications.clear(id);
+});
--- js/popup.js
+++ js/popup.js
@@ -1,120 +1,85 @@
-// Shortcut for chrome.extension.getBackgroundPage(). Allows me to execute background.js functions
+/* Shortcut for chrome.extension.getBackgroundPage(). Allows me to execute background.js functions */
 var background = chrome.extension.getBackgroundPage();
-var token;
-var socket;
+var radio = background.radio;
 
-background.storage.get(items => token = items.auth_token);
+setInfo();
+background.addEventListener('songChanged', setInfo);
 
-(function createSocket() {
+function setInfo() {
+	
+	var data = radio.data;
 
-	socket = new WebSocket('wss://listen.moe/api/v2/socket');
+	/* Sets Current Listners */
+	$('#current-listeners').text(data.listeners);
 
-	socket.onopen = function() {
-		console.log('Alright we got a connection. \\o/');
-		/* == Need to check if token is valid == */
-		if (token) {
-			checkAuth(token).then(() => {
-				/* == Sending the user token will bring back additional infomation == */
-				socket.send(JSON.stringify({token: token}));
-			}).catch(() => console.log('Invalid Token Bruh'));
+	/* Sets Now Playing Info */
+	$('#now-playing-info span').text(`${data.artist_name ? data.artist_name + ' -' : ''} ${data.song_name} ${data.anime_name ? '[' + data.anime_name + ']' : ''}`);
+
+	/* Sets Requester Info */
+	if (data.requested_by) {
+		if (/\s/g.test(data.requested_by)) {
+			$('#now-playing-request').show().text('🎉 ' + data.requested_by + ' 🎉');
+		} else {
+			$('#now-playing-request').show().text('Requested by ' + data.requested_by);
 		}
-	};
+	} else {
+		$('#now-playing-request').hide().text('');;
+	}
 
-	socket.onerror = function() {
-		console.log('Uhh. Something happened');
-		$('#now-playing-text').text('An error has occurred. Attempting to reconnect...');
-	};
+	/* Saves Song ID */
+	$('.toggle-favorite').data('id', data.song_id);
 
-	socket.onclose = function() {
-		console.log('Welp. The connection was closed :(');
-		console.log('Reconnecting...');
-		setTimeout(createSocket, 10000);
-	};
+	if (data.extended) {
 
-	socket.onmessage = function(response) {
+		$('#favs a').prop('href', 'https://listen.moe/#/favorites').text('View Favorites');
+		$('.toggle-favorite').show();
 
-		if (response.data !== '') {
+		if (data.extended.favorite)
+			$('.toggle-favorite').removeClass('glyphicon-star-empty');
+		else
+			$('.toggle-favorite').addClass('glyphicon-star-empty');
 
-			/* == Prepare the data == */
-			let data;
-			try {data = JSON.parse(response.data)}
-			catch (err) {console.error(err)}
-			if (!data) return;
+		if (data.extended.queue.songsInQueue !== 0) {
 
-			/* == Sets Current Listners == */
-			$('#current-listeners').text(data.listeners);
+			$('#queue-container').show();
+			$('#queue-amount').text(data.extended.queue.songsInQueue);
 
-			/* == Sets Now Playing Info  == */
-			$('#now-playing-info span').text(`${data.artist_name ? data.artist_name + ' -' : ''} ${data.song_name} ${data.anime_name ? '[' + data.anime_name + ']' : ''}`);
+			if (data.extended.queue.hasSongInQueue) {
 
-			/* == Sets Requester Info == */
-			if (data.requested_by) {
-				if (/\s/g.test(data.requested_by)) {
-					$('#now-playing-event').show();
-					$('#now-playing-event').text('🎉 ' + data.requested_by + ' 🎉');
-				} else {
-					$('#now-playing-request').show();
-					$('#request-username').prop('href', 'https://forum.listen.moe/u/' + data.requested_by).text(data.requested_by);
-				}
+				$('#queue-user-before, #queue-user').show();
+				$('#queue-amount').parent().prop('title', 'You have ' + data.extended.queue.userSongsInQueue + ' queued song(s)').css('cursor', 'help');
+
+				if (data.extended.queue.inQueueBeforeUserSong === 0) $('#queue-user-before-amount').text('The next song is yours!');
+				else if (data.extended.queue.inQueueBeforeUserSong === 1) $('#queue-user-before-amount').text('There is ' + data.extended.queue.inQueueBeforeUserSong + ' song before your next song.');
+				else $('#queue-user-before-amount').text('There is ' + data.extended.queue.inQueueBeforeUserSong + ' songs before your next song.');
+
 			} else {
-				$('#now-playing-request, #now-playing-event').hide();
-				$('#request-username').prop('href', '').text('');
+
+				$('#queue-user-before, #queue-user').hide();
+				$('#queue-user-before-amount, #queue-user-amount').text('N/A');
+				$('#queue-amount').parent().prop('title', '').css('cursor', 'default');
+
 			}
 
-			/* == Saves Song ID == */
-			$('.toggle-favorite').data('id', data.song_id);
+		} else {
 
-			if (data.extended) {
-
-				$('#favs a').prop('href', 'https://listen.moe/#/favorites').text('View Favorites');
-				$('.toggle-favorite').show();
-
-				if (data.extended.favorite)
-					$('.toggle-favorite').removeClass('glyphicon-star-empty');
-				else
-					$('.toggle-favorite').addClass('glyphicon-star-empty');
-
-				if (data.extended.queue.songsInQueue !== 0) {
-
-					$('#queue-container').show();
-					$('#queue-amount').text(data.extended.queue.songsInQueue);
-
-					if (data.extended.queue.hasSongInQueue) {
-
-						$('#queue-user-before, #queue-user').show();
-						$('#queue-amount').parent().prop('title', 'You have ' + data.extended.queue.userSongsInQueue + ' queued song(s)').css('cursor', 'help');
-
-						if (data.extended.queue.inQueueBeforeUserSong === 0) $('#queue-user-before-amount').text('The next song is yours!');
-						else if (data.extended.queue.inQueueBeforeUserSong === 1) $('#queue-user-before-amount').text('There is ' + data.extended.queue.inQueueBeforeUserSong + ' song before your next song.');
-						else $('#queue-user-before-amount').text('There is ' + data.extended.queue.inQueueBeforeUserSong + ' songs before your next song.');
-
-					} else {
-
-						$('#queue-user-before, #queue-user').hide();
-						$('#queue-user-before-amount, #queue-user-amount').text('N/A');
-						$('#queue-amount').parent().prop('title', '').css('cursor', 'default');
-
-					}
-
-				} else {
-
-					$("#queue-container").hide();
-					$('#queue-amount').text('N/A');
-					$('#queue-amount').parent().prop('title', '').css('cursor', 'default');
-
-				}
-
-			} else $('#favs a').prop('href', 'https://listen.moe/#/auth').text('Login');
+			$("#queue-container").hide();
+			$('#queue-amount').text('N/A');
+			$('#queue-amount').parent().prop('title', '').css('cursor', 'default');
 
 		}
+
+	} else {
+		
+		$('#favs a').prop('href', 'https://listen.moe/#/auth').text('Login');
 
 	}
 
-})();
+}
 
 $(function() {
 
-	// Does Scrolling Text
+	/* Does Scrolling Text */
 	var timeout;
 
 	timeout = setTimeout(autoScroll, 1000);
@@ -156,100 +121,56 @@ $(function() {
 		copyText(this.innerText);
 	});
 
-	// Initialize Volume Slider
+	/* Initialize Volume Slider */
 	$('#volume-slider').slider({
-		min: 0, max: 100, range: 'min', value: background.radio.getVol(),
+		min: 0, max: 100, range: 'min', value: radio.getVol(),
 		slide: function(event, ui) {
-			background.radio.setVol(Math.floor(ui.value));
+			radio.setVol(Math.floor(ui.value));
 		}
 	});
 
-	// Sets Play/Pause depending on player status
-	if ( background.radio.isPlaying() ) {
-		$('.playpause').removeClass('glyphicon-play');
-		$('.playpause').addClass('glyphicon-pause');
-	}
+	/* Sets Play/Pause depending on player status */
+	if (!radio.isPlaying()) $('.playpause').removeClass('glyphicon-pause');
 
-	// Enable/Disable Player
-	$(document).on('click', '.playpause', function() {
-		if ( background.radio.isPlaying() ) {
-			$('.playpause')
-				.removeClass('glyphicon-pause')
-				.addClass('glyphicon-play');
-			background.radio.disable();
+	/* Enable/Disable Player */
+	$('.playpause').click(function() {
+		if (radio.isPlaying()) {
+			$('.playpause').removeClass('glyphicon-pause');
+			radio.disable();
 		} else {
-			$('.playpause')
-				.removeClass('glyphicon-play')
-				.addClass('glyphicon-refresh');
-			background.radio.enable();
+			$('.playpause').addClass('glyphicon-pause');
+			radio.enable();
 		}
 	});
 
-	// Handles Play/Pause icons. Changes icon depending on the player status.
-	$(background.player).on('play abort loadstart', function(e) {
-		if (e.type === 'play') {
-			$('.playpause')
-				.removeClass('glyphicon-play')
-				.removeClass('glyphicon-refresh')
-				.addClass('glyphicon-pause');
-		}
-	});
-
-	// Random Ram or Rem
-	// Math.floor(Math.random() * 2) ? $('#ram_rem').css('background-position-x', '4px') : $('#ram_rem').css('background-position-x', '72px');
-
-	background.storage.get(function(items) {
-		// Does Autoplay checkbox
-		$('#radio-autoplay').prop('checked', items.ListenMoeAutoPlay).change(function() {
-			background.storage.set({ListenMoeAutoPlay: this.checked});
+	background.storage.get({ enableAutoplay: false }, function(items) {
+		$('#radio-autoplay').prop('checked', items.enableAutoplay).change(function() {
+			background.storage.set({ enableAutoplay: this.checked });
 		});
 	});
 
-	// Favorites Button
-	$(document).on('click', '.toggle-favorite', function() {
-		$.ajax({
-			url: 'https://listen.moe/api/songs/favorite',
-			type: 'POST',
-			headers: { 'Authorization': 'Bearer ' + token},
-			contentType: 'application/json;charset=utf-8',
-			data: JSON.stringify({ "song": $(this).data('id') }),
-			success: function(response) {
-				if (response.success) {
-					if (response.favorite) {
-						$('.toggle-favorite').removeClass('glyphicon-star-empty');
-					} else {
-						$('.toggle-favorite').addClass('glyphicon-star-empty');
-					}
-					//socket.send('update');
-				} else
-					console.log(error);
-			},
-			error: function(error) {
-				console.error(error);
-			}
-		});
+	/* Favorites Button */
+	$('.toggle-favorite').click(function() {
+		var songID = $(this).data('id');
+		background.radio.toggleFavorite(songID).then(function(favorited) {
+			if (favorited)
+				$('.toggle-favorite').removeClass('glyphicon-star-empty');
+			else
+				$('.toggle-favorite').addClass('glyphicon-star-empty');
+		}).catch(console.error);
 	});
+
+	/* Opens Keyboard Shortcuts */
+	$('#settings a').click(function() {
+		chrome.runtime.openOptionsPage();
+	});
+
+	/* Radom character */
+
 
 });
 
-/* == Checks to see if token is valid == */
-function checkAuth(token) {
-	return new Promise(function (fulfill, reject) {
-		$.ajax({
-			url: "https://listen.moe/api/user",
-			type: 'GET',
-			headers: { 'Authorization': 'Bearer ' + token},
-			success: function(response) {
-				fulfill(response);
-			},
-			error: function(error) {
-				reject(error);
-			}
-		});
-	});
-}
-
-/* == Copies text. Nothing more. == */
+/* Copies text. Nothing more. */
 function copyText(text) {
 	var input = document.createElement('textarea');
 	document.body.appendChild(input);
